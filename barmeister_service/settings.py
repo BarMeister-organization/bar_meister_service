9b8f064d1c5592cfb958ede865a689fdcad8f2c5
Alla Kuksa <alla.kuksa92@gmail.com>
2024-08-27 19:42:26 +0300

    Added djangorestframework

--- barmeister_service/settings.py
+++ barmeister_service/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "barmeister",
     "user",
 ]
@@ -111,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
