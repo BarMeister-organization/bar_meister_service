5608d6bdb1c62c1904885d55ecc1695028f95ed4
Alla Kuksa <alla.kuksa92@gmail.com>
2024-08-28 15:56:15 +0300

    Added simplejwt authentication

--- user/views.py
+++ user/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 
-# Create your views here.
+from barmeister.permissions import IsOwnerOrReadOnly
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+
+class ManageUserView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly, IsAdminUser]
+
+    def get_object(self):
+        return self.request.user
