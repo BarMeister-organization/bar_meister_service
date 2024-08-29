06d3dc970081b95cd67cc9ce290ff2777fa31793
Alla Kuksa <alla.kuksa92@gmail.com>
2024-08-29 18:25:22 +0300

    Added Cocktail and Ingredient models in Admin panel

--- barmeister/admin.py
+++ barmeister/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from barmeister.models import Ingredient, CocktailRecipe
 
-# Register your models here.
+admin.site.register(Ingredient)
+admin.site.register(CocktailRecipe)
