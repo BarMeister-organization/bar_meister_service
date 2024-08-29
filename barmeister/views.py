2d1b00c2267244ee9a7b24f09988f48f20ee388b
Alla Kuksa <alla.kuksa92@gmail.com>
2024-08-29 18:24:58 +0300

    Added CocktailRecipeViewSet and IngredientsViewSet

--- barmeister/views.py
+++ barmeister/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from barmeister.models import CocktailRecipe, Ingredient
+from barmeister.serializers import CocktailSerializer, IngredientSerializer
 
-# Create your views here.
+
+class CocktailRecipeViewSet(viewsets.ModelViewSet):
+    queryset = CocktailRecipe.objects.all()
+    serializer_class = CocktailSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class IngredientsViewSet(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
