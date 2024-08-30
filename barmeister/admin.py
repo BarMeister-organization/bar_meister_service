3c4fc31206627de2290dfe8f4e7a71fb041c9527
Alla Kuksa <alla.kuksa92@gmail.com>
2024-08-30 17:13:50 +0300

    Added FavouriteCocktails model

--- barmeister/admin.py
+++ barmeister/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from barmeister.models import Ingredient, CocktailRecipe
+from barmeister.models import Ingredient, CocktailRecipe, Comment, FavouriteCocktails
+
 
 admin.site.register(Ingredient)
 admin.site.register(CocktailRecipe)
+admin.site.register(Comment)
+admin.site.register(FavouriteCocktails)
