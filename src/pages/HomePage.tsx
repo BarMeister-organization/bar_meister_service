187373f05987c2042eeb52e829dde1892070352d
Valentyn Sobol <valiksobol14@gmail.com>
2024-09-16 12:43:59 +0300

    add filters, custom shake block

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import { CustomShake } from "../components/CustomShake";
+import { FilterByComponents } from "../components/FilterByComponents";
 import Hero from "../components/Hero/Hero";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <FilterByComponents />
       <CustomShake />
     </>
   );
